2787be099a7cacc5328ace8cf71b04b567c1a8ca
unknown <rma013@uottawa.com>
2021-03-03 13:09:48 -0500

    fix error

--- pages/Error.cshtml.cs
+++ pages/Error.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace Cv19projectges
+namespace cv19project.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
