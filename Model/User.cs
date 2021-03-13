451087dd2da516ad64c310473a225cc22129d9f0
Mitch <mbate052@uottawa.ca>
2021-03-13 00:44:35 +0000

    added UID to users model

--- Model/User.cs
+++ Model/User.cs
@@ -1,5 +1,6 @@
 namespace covidwatch.Model {
     public class User {
+        public int UID {get; set;}
         public string Username {get; set;}
         public string Password { get; set; }
     }
