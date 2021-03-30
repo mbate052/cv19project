7bda17b36041505fd441d9fcdb06a8900d52526c
mbate052 <78161066+mbate052@users.noreply.github.com>
2021-03-30 18:41:27 +0000

    changed "prevention" page to "view users"

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using covidwatch.Model;
+using covidwatch.services;
 using covidwatch.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,21 +14,32 @@ namespace covidwatch.Pages
     public class CreateModel : PageModel
     {
         private readonly ILogger<CreateModel> _logger;
+        /// private JsonUserReaderService Jsonservice;
         public UsersService DBservice;
+        public IEnumerable<User> Users; 
+        public AssessmentCenterService ACservice;
+        public IEnumerable<AssessmentCenter> AssessmentCenters;
+        
         public string Username { get; set; }
         public string Password { get; set; }
-        public int UID { get; set; }
+        private object jsonservice;
 
-        public CreateModel(ILogger<CreateModel> logger, UsersService uServ)
+        public CreateModel(ILogger<CreateModel> logger, JsonUserReaderService jurs, UsersService uServ, AssessmentCenterService aServ) 
         {
             _logger = logger;
+            jsonservice = jurs;
             DBservice = uServ;
+            ACservice = aServ;
+        }
+        public void OnGet()
+        {
+            Users = DBservice.Get();
+            AssessmentCenters = ACservice.Get();
         }
         public void OnPostSubmit(User newuser)
         {
             this.Username = newuser.Username;
             this.Password = newuser.Password;
-            this.UID = newuser.UID;
             DBservice.Create(newuser);
     
         }
